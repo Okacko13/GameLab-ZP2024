04ebdc3d93513b3fb83c6fe52d94a35f1f65a010
Okacko13 <ondator.007@gmail.com>
2024-04-05 17:46:29 +0200

    Created new method to class Frame named settupFrame(). It sets the JFrame.

--- src/Frame.java
+++ src/Frame.java
@@ -1,10 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame {
 
     private JFrame frame;
 
+    public Frame() {
 
+        settupFrame();
+
+    }
+
+    public void settupFrame(){
+
+        frame = new JFrame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+        frame.getContentPane().setBackground(Color.BLACK);
+        frame.setVisible(true);
+
+    }
 
 
 }

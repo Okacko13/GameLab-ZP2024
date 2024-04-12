9eb2a451eba83bdb42860f73df13390160162da3
Okacko13 <ondator.007@gmail.com>
2024-04-12 15:02:09 +0200

    Created new constructor for class TictacToe

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+import java.util.Random;
+
 public class TicTacToe {
+
+    private Random random;
+
+    private JButton[] buttons;
+
+    public TicTacToe() {
+
+        buttons = new JButton[9];
+
+    }
 }

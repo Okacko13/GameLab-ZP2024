93f8a61eb54cc82a39197cbb3fd844ddaadf5907
Okacko13 <ondator.007@gmail.com>
2024-04-12 14:47:25 +0200

    Created method to settup game opening buttons Implemented ActionListener to this class and generated actionPerformed method

--- src/Frame.java
+++ src/Frame.java
@@ -1,11 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Frame {
+public class Frame extends JFrame implements ActionListener {
 
-    private JFrame frame;
+    private JLabel jLabel;
+
+    private JButton[] gameOpener;
+
+    private ImageIcon imageIcon;
 
     public Frame() {
+
+        jLabel = new JLabel();
+        gameOpener = new JButton[3];
 
         settupFrame();
 
@@ -13,14 +22,55 @@ public class Frame {
 
     public void settupFrame(){
 
-        frame = new JFrame();
+        setTitle("Lobby");
+        imageIcon = new ImageIcon("LOBBY-LOGO.png");
+        setIconImage(imageIcon.getImage());
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,800);
-        frame.getContentPane().setBackground(Color.BLACK);
-        frame.setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setSize(750,750);
+        setResizable(false);
+
+        getContentPane().setBackground(new Color(57, 57, 57));
+
+        this.settupButtons();
+        setVisible(true);
 
     }
 
+    public void settupButtons(){
+
+        for(int i = 0; i < gameOpener.length; i++){
+
+            gameOpener[i] = new JButton();
+            gameOpener[i].addActionListener(this);
+            gameOpener[i].setFocusable(false);
+            gameOpener[i].setBackground(new Color(255, 41, 41, 148));
+
+        }
+
+        gameOpener[0].setBounds(50,375,150,150);
+        gameOpener[1].setBounds(300,375,150,150);
+        gameOpener[2].setBounds(550,375,150,150);
+
+        this.add(gameOpener[0]);
+        this.add(gameOpener[1]);
+        this.add(gameOpener[2]);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (gameOpener[0].equals(e.getSource())) {
+            System.out.println("tictactoe");
+
+        } else if(gameOpener[1].equals(e.getSource())){
+            System.out.println("mastermind");
+
+        } else if(gameOpener[2].equals(e.getSource())){
+            System.out.println("quoridors");
+
+        }
+    }
 
 }

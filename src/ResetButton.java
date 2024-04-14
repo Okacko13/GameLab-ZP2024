0d49147cd754525fb2f524bd93515cf49536ceaa
Okacko13 <ondator.007@gmail.com>
2024-04-14 15:48:18 +0200

    created new variable private JButton resetToLobby

--- src/ResetButton.java
+++ src/ResetButton.java
@@ -1,4 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class ResetButton extends JPanel {
+
+    private JButton resetToLobby;
+
+    public ResetButton() {
+
+        setSize(100,100);
+        setLocation(50,50);
+        setBackground(new Color(118, 70, 215));
+        setLayout(null);
+
+
+    }
+
+
+
 }

ba577740e2f4f1139c70e29ef9d2c2a87c093d85
Okacko13 <ondator.007@gmail.com>
2024-04-14 16:12:09 +0200

    created new method public void setUpResetB() that sets up reset button. implemented ActionListener and generated required methods

--- src/ResetButton.java
+++ src/ResetButton.java
@@ -1,24 +1,42 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ResetButton extends JPanel {
+public class ResetButton extends JPanel implements ActionListener {
 
     private JButton resetToLobby;
 
     public ResetButton() {
 
-        setSize(100,100);
+        setSize(200,100);
         setLocation(50,50);
         setBackground(new Color(118, 70, 215));
         setLayout(null);
 
     }
 
+    public void setUpResetB(){
+        resetToLobby = new JButton("back to lobby");
+        resetToLobby.setBounds(5,5,180,90);
+        resetToLobby.setIcon(null);
+        resetToLobby.setBackground(new Color(215, 191, 70));
+        resetToLobby.setBorderPainted(false);
+        resetToLobby.setFont(new Font("High game font",Font.PLAIN,20));
+        resetToLobby.setForeground(new Color(0,0,0));
+        resetToLobby.setFocusable(false);
+        resetToLobby.addActionListener(this);
+        resetToLobby.setVisible(true);
+
+    }
 
     public void setVisibility(Boolean visibility){
         setVisible(visibility);
     }
 
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
+    }
 }

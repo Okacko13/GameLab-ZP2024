8f79eea19456b4a71a4b765f57863c257ff53a3c
Okacko13 <ondator.007@gmail.com>
2024-04-18 15:35:45 +0200

    added new settings to method settupButtons. Buttons are now better :D

--- src/Frame.java
+++ src/Frame.java
@@ -48,12 +48,17 @@ public class Frame extends JFrame implements ActionListener {
             gameOpener[i].addActionListener(this);
             gameOpener[i].setFocusable(false);
             gameOpener[i].setBackground(new Color(255, 41, 41, 148));
+            gameOpener[i].setForeground(new Color(0, 0, 0));
 
         }
 
         gameOpener[0].setBounds(50,375,150,150);
         gameOpener[1].setBounds(300,375,150,150);
         gameOpener[2].setBounds(550,375,150,150);
+
+        gameOpener[0].setText("TicTacToe");
+        gameOpener[1].setText("Mastermind");
+        gameOpener[2].setText("Quoridor");
 
         this.add(gameOpener[0]);
         this.add(gameOpener[1]);

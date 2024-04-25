d054203745073afdefcb5889a5381c72f60bbe3b
Okacko13 <ondator.007@gmail.com>
2024-04-25 14:38:10 +0200

    created new method public void setUpPanel()

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
+
+
+
+
+    public void setUpPanel(){
+        setBounds(5,5,740,740);
+        setLayout(null);
+        setBackground(Color.BLACK);
+    }
+
+
 }

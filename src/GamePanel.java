8fcc318a79be721ce3fa297d9055bd6b80b1263f
Okacko13 <ondator.007@gmail.com>
2024-04-25 14:39:41 +0200

    generated constructor and setting up panel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,8 +3,9 @@ import java.awt.*;
 
 public class GamePanel extends JPanel {
 
-
-
+    public GamePanel() {
+        setUpPanel();
+    }
 
     public void setUpPanel(){
         setBounds(5,5,740,740);

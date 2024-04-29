d997378fa11305981f4b83472662bd804c74b311
Okacko13 <ondator.007@gmail.com>
2024-04-29 20:36:40 +0200

    Created new 2 new methods - public void buttonInitialize, public void initializePanel

--- src/TICTACTOE/ResetTicTacToeButton.java
+++ src/TICTACTOE/ResetTicTacToeButton.java
@@ -1,6 +1,7 @@
 package TICTACTOE;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,11 +11,35 @@ public class ResetTicTacToeButton extends JPanel implements ActionListener {
 
     private TicTacToe game;
 
-    public ResetTicTacToeButton() {
+    public ResetTicTacToeButton(TicTacToe game) {
+        this.game = game;
+        buttonInitialize();
+        initializePanel();
+        add(resetButton);
+        setVisible(true);
+    }
+
+    public void buttonInitialize(){
+        resetButton = new JButton("RESET GAME");
+        resetButton.setFont(new Font("Arial", Font.BOLD,35));
+        resetButton.setBounds(10,10,185,85);
+        resetButton.setBackground(Color.DARK_GRAY);
+        resetButton.setForeground(Color.WHITE);
+
+
+    }
+
+    public void initializePanel(){
+        this.setBackground(Color.BLACK);
+        this.setBounds(0,0,200,100);
+        this.setLayout(null);
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(resetButton == e.getSource()){
+            game.newGame();
+        }
     }
 }

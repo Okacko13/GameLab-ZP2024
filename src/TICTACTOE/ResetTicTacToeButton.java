008432d8e977ac05c043cef4200dee5b8dc4bf65
Okacko13 <ondator.007@gmail.com>
2024-04-29 14:47:55 +0200

    Created new class ResetTicTacToeButton

---
+++ src/TICTACTOE/ResetTicTacToeButton.java
@@ -0,0 +1,18 @@
+package TICTACTOE;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ResetTicTacToeButton extends JPanel implements ActionListener {
+
+    private JButton resetButton;
+
+    public ResetTicTacToeButton() {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}

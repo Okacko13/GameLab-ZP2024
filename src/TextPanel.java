2af26b5dd708f7ab517a9de2052c46abc133ced9
Okacko13 <ondator.007@gmail.com>
2024-04-30 21:41:26 +0200

    created all logics for class TextPanel

--- src/TextPanel.java
+++ src/TextPanel.java
@@ -1,4 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class TextPanel extends JPanel {
+
+    private JLabel texPanel;
+
+    public TextPanel() {
+        initializeTextPanel();
+        initializePanel();
+
+    }
+
+    public void initializeTextPanel(){
+        texPanel = new JLabel();
+        texPanel.setBackground(Color.DARK_GRAY);
+        texPanel.setForeground(Color.WHITE);
+        texPanel.setFont(new Font("Times new roman",Font.BOLD,30));
+        texPanel.setFocusable(false);
+    }
+
+    public void initializePanel(){
+        setBackground(Color.BLACK);
+        setLayout(null);
+        add(texPanel);
+        setVisible(true);
+    }
+
+    public void setTextOnPanel(String text){
+        texPanel.setText(text);
+    }
 }

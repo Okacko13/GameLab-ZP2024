e2be890371a91aa4542ee54844f18ebbe47da2f4
Okacko13 <ondator.007@gmail.com>
2024-05-03 07:35:12 +0200

    Created new class FramePanel extending JPanel. This class is going to be used as panel for game projector and upper panel display. Class has variables: private GamePanel gamepanel, protected TextPanel textPanel, private ResetTicTacToeButton resetTTT, private ResetButton backToLobby, pirvate JPanel top. Class has methods: void turnOffFramePanel(), void setVisibility(boolean bool), void setTop(), void setFramePanel(), void startTicTacToe

---
+++ src/FRAME_COMPONENTS/FramePanel.java
@@ -0,0 +1,68 @@
+package FRAME_COMPONENTS;
+
+import TICTACTOE.ResetTicTacToeButton;
+import TICTACTOE.TicTacToe;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FramePanel extends JPanel {
+
+    private GamePanel gamePanel;
+    protected TextPanel textPanel;
+    private ResetTicTacToeButton resetTTT;
+    private ResetButton backToLobby;
+
+    private JPanel top;
+
+    public FramePanel(GamePanel gamePanel, TextPanel textPanel, ResetTicTacToeButton resetTTT, ResetButton backToLobby) {
+        this.gamePanel = gamePanel;
+        this.textPanel = textPanel;
+        this.resetTTT = resetTTT;
+        this.backToLobby = backToLobby;
+        backToLobby.setFramePanel(this);
+
+        setTop();
+        add(gamePanel,BorderLayout.CENTER);
+        setFramePanel();
+    }
+
+    public void turnOffFramePanel(){
+        this.setVisible(false);
+    }
+
+    public void setVisibility(boolean bool){
+        setVisible(bool);
+    }
+    public void setTop(){
+        top = new JPanel();
+        top.setBounds(0,0,750,150);
+        top.setBackground(Color.BLACK);
+        top.setLayout(null);
+        top.add(backToLobby);
+        top.add(resetTTT);
+        top.add(textPanel);
+        top.setVisible(false);
+
+        textPanel.setTextOnPanel("Tic-Tac-Toe");
+
+        this.add(top,BorderLayout.NORTH);
+    }
+    public void setFramePanel(){
+        setBounds(0,0,750,750);
+        setLayout(new BorderLayout());
+        setVisible(false);
+    }
+
+    public void startTicTacToe(TicTacToe ticTacToe){
+        ticTacToe.setTextPanel(this.textPanel);
+
+        gamePanel.setVisibility(true);
+        gamePanel.startTicTacToe(ticTacToe);
+        resetTTT.setTicTacToe(ticTacToe);
+        top.setVisible(true);
+        setVisibility(true);
+    }
+
+
+}

95df2ec1dfa6d1448c16f9f1f7db9b0af5f2a6b9
Okacko13 <ondator.007@gmail.com>
2024-05-03 07:38:59 +0200

    Created new class ResetButton that will move player back to lobby

---
+++ src/FRAME_COMPONENTS/ResetButton.java
@@ -0,0 +1,64 @@
+package FRAME_COMPONENTS;
+
+import FRAME_COMPONENTS.Frame;
+import FRAME_COMPONENTS.FramePanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ResetButton extends JPanel implements ActionListener {
+
+    private JButton resetToLobby;
+    private FramePanel framePanel;
+    private FRAME_COMPONENTS.Frame frame;
+
+    public ResetButton(FramePanel framePanel, Frame frame) {
+        this.frame = frame;
+        this.framePanel = framePanel;
+
+        setUpPanel();
+        setUpResetB();
+
+    }
+
+    public void setFramePanel(FramePanel framePanel){
+        this.framePanel = framePanel;
+    }
+
+    public void setUpPanel(){
+        setSize(200,150);
+        setLocation(0,0);
+        setBackground(new Color(72, 67, 66));
+        setLayout(null);
+    }
+
+    public void setUpResetB(){
+        resetToLobby = new JButton("LOBBY");
+        resetToLobby.setBounds(10,10,180,130);
+        resetToLobby.setBackground(new Color(114, 0, 255, 255));
+        resetToLobby.setFont(new Font("Arial",Font.BOLD,40));
+        resetToLobby.setForeground(Color.WHITE);
+        resetToLobby.setFocusable(false);
+        resetToLobby.setBorder(null);
+        resetToLobby.addActionListener(this);
+        resetToLobby.setVisible(true);
+
+        add(resetToLobby);
+
+    }
+
+    public void setVisibility(Boolean visibility){
+        setVisible(visibility);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(resetToLobby.equals(e.getSource())){
+            framePanel.turnOffFramePanel();
+            frame.enableGameOButtons(true);
+        }
+    }
+}

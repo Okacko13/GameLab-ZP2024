aff8b7b532ceb2e1da48d71832cef1dd2a5b1291
Okacko13 <ondator.007@gmail.com>
2024-05-08 18:49:03 +0200

    Created lot off logics

--- src/MASTERMIND/Row.java
+++ src/MASTERMIND/Row.java
@@ -2,7 +2,7 @@ package MASTERMIND;
 
 public class Row {
 
-    private Pin[] pins;
+    protected Pin[] pins;
 
     public Row() {
         initializePins();
@@ -25,18 +25,58 @@ public class Row {
         }
 
     }
+    public void setPinByIndex(int i,Colors colors){
 
-    public void setPinOne(Colors color){
-        pins[0].setColor(color);
+        switch (i){
+            case 0:
+                pins[0].setColor(colors);
+                break;
+            case 1:
+                pins[1].setColor(colors);
+                break;
+            case 2:
+                pins[2].setColor(colors);
+                break;
+            case 3:
+                pins[3].setColor(colors);
+                break;
+        }
+
     }
-    public void setPinTwo(Colors color){
-        pins[1].setColor(color);
+
+    public Pin getPin(int index){
+        return pins[index];
     }
-    public void setPinThree(Colors color){
-        pins[2].setColor(color);
+
+    public boolean checkSameColorInRow(Row row){
+
+        for (int i = 0; i < row.pins.length ; i++) {
+            switch (i){
+                case 0 :
+                    if(row.getPin(i).getColor().equals(row.getPin(1).getColor()) || row.getPin(i).getColor().equals(row.getPin(2).getColor()) || row.getPin(i).getColor().equals(row.getPin(3).getColor())) return true;
+                    break;
+                case 1 :
+                    if(row.getPin(i).getColor().equals(row.getPin(0).getColor()) || row.getPin(i).getColor().equals(row.getPin(2).getColor()) || row.getPin(i).getColor().equals(row.getPin(3).getColor())) return true;
+                    break;
+                case 2 :
+                    if(row.getPin(i).getColor().equals(row.getPin(0).getColor()) || row.getPin(i).getColor().equals(row.getPin(1).getColor()) || row.getPin(i).getColor().equals(row.getPin(3).getColor())) return true;
+                    break;
+                case 3 :
+                    if(row.getPin(i).getColor().equals(row.getPin(0).getColor()) || row.getPin(i).getColor().equals(row.getPin(1).getColor()) || row.getPin(i).getColor().equals(row.getPin(2).getColor())) return true;
+                    break;
+            }
+        }
+
+        return false;
     }
-    public void setPinFour(Colors color){
-        pins[3].setColor(color);
+    public int numberOfColoredPins(Row row){
+        int numberOfColoredPins = 0;
+
+        for (int i = 0; i < pins.length; i++) {
+            if(!row.getPin(i).getColor().equals(Colors.NONE)) numberOfColoredPins++;
+        }
+
+        return numberOfColoredPins;
     }
 
 }

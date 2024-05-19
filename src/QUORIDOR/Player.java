2f69eb08f5a7d4ceaf859bda4d5d9b93ec44601a
Okacko13 <ondator.007@gmail.com>
2024-05-19 11:21:27 +0200

    Created needed logics for Player

--- src/QUORIDOR/Player.java
+++ src/QUORIDOR/Player.java
@@ -6,6 +6,10 @@ import java.awt.*;
 public class Player extends JPanel {
 
     private final Color color;
+    private int numberOfWalls = 8;
+
+    private int coordinationX;
+    private int coordinationY;
 
     public Player(Color color) {
         this.color = color;
@@ -17,8 +21,33 @@ public class Player extends JPanel {
         setLocation(5,5);
         setBackground(color);
     }
+    public int getNumebreOfWalls(){
+        return numberOfWalls;
+    }
 
+    public void placeWall(){
+        if(numberOfWalls > 0){
+            numberOfWalls--;
+        }
+    }
 
+    public int getCoordinationX() {
+        return coordinationX;
+    }
 
+    public void setCoordinationX(int move) {
+        if(move - 1 == coordinationX || move + 1 == coordinationX){
+            this.coordinationX = move;
+        }
+    }
 
+    public int getCoordinationY() {
+        return coordinationY;
+    }
+
+    public void setCoordinationY(int move) {
+        if(move - 1 == coordinationX || move + 1 == coordinationX){
+            this.coordinationY = move;
+        }
+    }
 }

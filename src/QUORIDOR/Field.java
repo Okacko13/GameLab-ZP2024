2a73bfe12a1082b9285d95a8593eeb020f115b72
Okacko13 <ondator.007@gmail.com>
2024-05-19 11:17:41 +0200

    Created basic logics

--- src/QUORIDOR/Field.java
+++ src/QUORIDOR/Field.java
@@ -2,14 +2,26 @@ package QUORIDOR;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Field extends JPanel {
+public class Field extends JPanel implements ActionListener {
 
     private Player player;
-    private Color background;
+    private GameField gameField;
 
-    public Field() {
+    public Field(GameField gameField) {
+        this.gameField = gameField;
+        initialize();
+    }
 
+    public void initialize(){
+        setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
+        setBackground(Color.DARK_GRAY);
+        setLayout(null);
+    }
+    public void setVisibility(boolean bool){
+        setVisible(bool);
     }
 
     public void addPlayer(Player player){
@@ -20,4 +32,8 @@ public class Field extends JPanel {
         this.player = null;
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }

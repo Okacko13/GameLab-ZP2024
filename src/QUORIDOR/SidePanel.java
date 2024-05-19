54d2bcd2b732e5cc151946d31d16e2488cbdf432
Okacko13 <ondator.007@gmail.com>
2024-05-19 11:24:25 +0200

    Created working logics for SidePanel

--- src/QUORIDOR/SidePanel.java
+++ src/QUORIDOR/SidePanel.java
@@ -1,4 +1,32 @@
 package QUORIDOR;
 
-public class SidePanel {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class SidePanel extends JPanel {
+
+    private MoveButton moveButton;
+    private PlaceWallButton placeWallButton;
+
+    public SidePanel() {
+
+    }
+
+    public void initializePanel(ActionListener actionListener){
+        moveButton = new MoveButton();
+        placeWallButton = new PlaceWallButton();
+
+        moveButton.createMoveButton(actionListener);
+        placeWallButton.createMoveButton(actionListener);
+
+        this.setBounds(600,0,150,600);
+        this.setBackground(Color.DARK_GRAY);
+        this.setLayout(null);
+
+        this.add(moveButton);
+        this.add(placeWallButton);
+        setVisible(true);
+    }
+
 }

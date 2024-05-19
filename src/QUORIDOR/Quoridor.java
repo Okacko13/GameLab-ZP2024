0fe60717232f2c9a2097caadf958fa0aa62ed204
Okacko13 <ondator.007@gmail.com>
2024-05-19 11:22:04 +0200

    Made some basic stuff in class Quoridor

--- src/QUORIDOR/Quoridor.java
+++ src/QUORIDOR/Quoridor.java
@@ -1,20 +1,38 @@
 package QUORIDOR;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Quoridor implements ActionListener {
+public class Quoridor extends JPanel implements ActionListener {
 
-    private Player player1;
-
-    private Player player2;
-
-    private GameField gameField;
+    private Player playerOnTurn;
 
     private GamePanel gamePanel;
 
     private SidePanel sidePanel;
+
+    public Quoridor() {
+        initializeMailPanel();
+    }
+
+    public void initializeMailPanel(){
+        setSize(750,600);
+        setLocation(0,0);
+        setBackground(Color.YELLOW);
+        setLayout(null);
+
+        sidePanel = new SidePanel();
+        sidePanel.initializePanel(this);
+
+        gamePanel = new GamePanel();
+        gamePanel.initializePanel(600,600,0,0,new Player(Color.BLUE),new Player(Color.RED));
+
+        add(sidePanel);
+        add(gamePanel);
+        setVisible(true);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
